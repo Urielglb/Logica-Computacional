0392025319399ab1968a5e29eebcd83f4fc1cc37
Valencia <davidalencia@gmail.com>
2020-03-05 11:27:51 -0600

    LConj casi listo falta union2 y subconj  | 11:27 03/05/20

--- Práctica02/LConj.hs
+++ Práctica02/LConj.hs
@@ -1,5 +1,5 @@
 {-
-- Logica Conmputacional 2020-2 
+- Logica Conmputacional 2020-2
 - Implementación del Conjunto.
 - Profesor: Favio Ezequiel Miranda Perea
 - Ayudante: Alejandra Krystel Coloapa Díaz
@@ -10,29 +10,29 @@ module LConj where
 
 -- | equivL. Función que determina si dos conjuntos son equivalentes.
 equivL :: Eq a => [a] -> [a] -> Bool
-equivL l1 l2 = error "Funcion a implementar"
+equivL l1 l2 = and [(length l1) == (length l2),  (length l1) == (length (inter l1 l2))]
 
 -- | filtro. Función que dado un predicado y una lista l, devuelve
 -- la lista de los elementos de l que cumplen con el predicado.
 filtro :: (a -> Bool) -> [a] -> [a]
-filtro p m = error "Funcion a implementar"
+filtro p m = [x | x<-m, p(x)]
 
 -- | diff. Función que devuelve la diferencia de listas.
 diff :: Eq a => [a] -> [a] -> [a]
-diff l1 l2 = error "Funcion a implementar"
+diff l1 l2 = [x | x<-l1, not (elem x l2)]
 
 -- | union. Función que devuelve la union de listas.
 union :: Eq a => [a] -> [a] -> [a]
-union l m = error "Funcion a implementar" 
+union l m = [x | x<-l, not (elem x m)]++m
 
 -- / union2. Funcion que devuelve la union de una lista de lista [[1],[2]] = [1,2]
 union2 :: Eq a => [[a]] -> [a]
-union2 l = error "Funcion a implementar"
+union2 l =[y | x<-l, y<-x] -- / No esta completo. No resvisa repeticiones
 
 -- | inter. Función que devuelve la intersección de listas.
 inter :: Eq a => [a] -> [a] -> [a]
-inter l1 l2 = error "Funcion a implementar"
+inter l1 l2 =  [x |x<-l1, y<-l2, x==y]
 
--- | subconj. Función que devuelve la potencia de una lista
+-- / subconj. Función que devuelve la potencia de una lista
 subconj :: [a] -> [[a]]
-subconj l = error "Funcion a implementar"
+subconj  = error "aaaaaaaaaaaaaaaahhhhhhhhhhhhhhh!!!"
