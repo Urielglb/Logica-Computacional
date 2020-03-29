59025af5d843543ce4f99e9c77a2c4d3f6cc4fb3
Valencia <davidalencia@gmail.com>
2020-03-29 17:27:16 -0600

    Avances  | 17:27 03/29/20

--- Práctica02Parte2/DPLL.hs
+++ Práctica02Parte2/DPLL.hs
@@ -1,5 +1,5 @@
 {-
-- Logica Conmputacional 2020-2 
+- Logica Conmputacional 2020-2
 - Practica02 Parte 1, Implementación del algoritmo dpll.
 - Profesor: Dr. Favio Ezequiel Miranda Perea
 - Ayudante: Alejandra Krystel Coloapa Díaz
@@ -10,6 +10,7 @@ module DPLL where
 
 import LProp
 import Data.List
+import Control.Exception
 
 type Literal = Prop
 type Clausula = [Literal]
@@ -17,34 +18,82 @@ type Formula = [Clausula]
 type Modelo = [Literal]
 type Solucion = (Modelo, Formula)
 
+data MyException = FAIL
+     deriving Show
+instance Exception MyException
+
+
+
+obtenSolucion:: [Solucion]-> Solucion
+
+obtenSolucion [] =  throw FAIL
+obtenSolucion (x:l)
+  | success x = x
+  | otherwise = obtenSolucion l
+
+noContiene:: Literal -> Modelo -> Bool
+noContiene l [] = False
+noContiene l [x]
+        | l == x = False
+        | otherwise = True
+noContiene l (x:xs)
+        | l==x = False
+        | otherwise = noContiene l xs
+
+
 -- Seccion de funciones para la regla de la clausula unitaria
 esLiteral :: Clausula -> Bool
 esLiteral [x] = True
 esLiteral m = False
 
+allUnit::Solucion ->[Solucion]
+allUnit s@(m,f) = case [head l | l <- f, esLiteral l] of
+        []->[s]
+        v -> case [b | b<-v, not (negElem b m)] of
+                [] -> [s]
+                w -> [((m `union` [a]), f \\ [[a]])| a <- w]
+
 unit :: Solucion -> Solucion
-unit (m,[]) = (m,[])
-unit (m,x:xs)
-        | esLiteral x = (m++x,xs)
-        | otherwise = (mod,x:form)
-            where (mod,form) = unit(m,xs)
+unit s@(m,f) = case [head l | l <- f, esLiteral l] of
+        []->s
+        v -> case [b | b<-v, not (negElem b m)] of
+                [] -> s
+                w -> obtenSolucion([dpll((m `union` [a]), f \\ [[a]])| a <- w])
 
 --  Seccion de funciones para la regla de eliminacion
 
+elimLit :: Literal-> Formula -> Formula
+elimLit lit form = [c | c<-form, not (elem lit c)]
+
+allElim :: Solucion -> [Solucion]
+allElim s@(m,[]) = [s]
+allElim (m, f) =[ (m,  elimLit lit f) | lit<-m]
+
+
 elim :: Solucion -> Solucion
-elim ([],m) = ([],m)
-elim(m,[]) = (m,[])
-elim (m, f) = (m, [c| c<-f, [y| y<-c, elem y m]==[]])
+elim s@(m,[]) = s
+elim (m, f) = obtenSolucion([dpll (m,  elimLit lit f) | lit<-m])
+
 
 -- Seccion de funciones para la regla de reduccion
 
 redC :: Modelo->Clausula->Clausula
 redC m c = [clau | clau<-c, not(negElem clau m)]
 
+redLit:: Literal->Formula->Formula
+redLit l f = [[lit | lit<-c, not (negElem l [lit])] | c<-f]
+
+negElem:: Literal->Clausula->Bool
+negElem (Neg l) c =  elem l c
+negElem l c =  elem (Neg l) c
+
+allRed::Solucion->[Solucion]
+allRed s@(m, []) = [s]
+allRed (m, f) = [(m,  redLit lit f) | lit<-m]
+
 red :: Solucion -> Solucion
 red s@(m, []) = s
-red (m, c:f) = (m, (redC m c):reducido)
-        where (_, reducido) = red (m, f)
+red (m, f) = obtenSolucion([dpll (m,  redLit lit f) | lit<-m])
 
 -- Seccion de funciones para la regla de separacion
 lc :: Literal -> Literal
@@ -55,9 +104,22 @@ literales:: Formula -> [Literal]
 literales[] = []
 literales (y:ys) = union y (literales ys)
 
+quitaNeg:: [Literal]-> [Literal]
+quitaNeg [] = []
+quitaNeg ((Neg x):xs) = x:(quitaNeg xs)
+quitaNeg (x:xs) = x:(quitaNeg xs)
+
+allSplit:: Solucion->[Solucion]
+allSplit s@(m,f) = case [l | l<- (nub $ quitaNeg $ literales f), not(elem l m), not(negElem l m)]of
+                  []->[s]
+                  v -> [(lit:m, f) | lit<-v]++[((Neg lit):m, f) | lit<-v]
+
 
 split :: Solucion -> Solucion
-split s@(m,f) = error "Funcion a implementar"
+split s@(m, []) = s
+split s@(m,f) = case [l | l<- (nub $ quitaNeg $ literales f), not(elem l m), not(negElem l m)]of
+                  []->s
+                  v -> obtenSolucion([dpll (lit:m, f) | lit<-v]++[dpll ((Neg lit):m, f) | lit<-v])
 
 -- Seccion de funciones para la regla de conflicto
 
@@ -74,10 +136,14 @@ success(m,f) = False
 -- Seccion de las funciones principales de DPLL
 
 dpllsearch :: Solucion -> Solucion
-dpllsearch (m,f) = error "Funcion a implementar"
+dpllsearch s@(m,f) = split $ red $ elim $ unit s
 
 dpll :: Solucion -> Solucion
-dpll (m,f) = error "Funcion a implementar"
+dpll s@(m,f) = case success s of
+    True -> s
+    False ->  case conflict s of
+        True -> s
+        False -> dpll (dpllsearch s)
 
 main :: Solucion -> Solucion
 main s = error "Funcion a implementar"
@@ -86,10 +152,10 @@ main s = error "Funcion a implementar"
 
 bueno = [[Neg (V "P"), V "R", Neg (V "T")],[Neg (V "Q"), Neg (V "R")],[V "P",Neg (V "S")],[Neg (V "P"), V "Q", Neg (V "R"), Neg (V "S")]]
 exe1 = [[V "p", V "q"],[Neg (V "q")],[Neg (V "p"), V "q", Neg (V "r")]]
-exe2 = [[V "p", V "q"],[V "p", Neg (V "q")],[V "r", V "q"],[V "r", Neg (V "q")]]    
+exe2 = [[V "p", V "q"],[V "p", Neg (V "q")],[V "r", V "q"],[V "r", Neg (V "q")]]
 exe3 = [[V "p", Neg (V "q")],[Neg (V "p"), V "q"],[V "q", Neg (V "r")],[Neg (V "q"), Neg (V "r")]]
 exe4 = [[V "p", V "q"], [V "r", Neg (V "q"), Neg (V "s")], [Neg (V "p"), V "s"], [Neg (V "r")]]
-exe5 = [[V "p", V "q", V "r"], 
+exe5 = [[V "p", V "q", V "r"],
         [V "p", Neg (V "q"), Neg (V "r")],
         [V "p", Neg (V "w")],
         [Neg (V "q"), Neg (V "r"), Neg (V "w")],
@@ -98,12 +164,18 @@ exe5 = [[V "p", V "q", V "r"],
         [V "u", V "x"],
         [V "q", Neg (V "u")],
         [Neg (V "r"), Neg (V "u")]]
-exe6 = [[V "p"], [Neg (V "p")]]        
+exe6 = [[V "p"], [Neg (V "p")]]
 
 ejemplo1 = main ([], exe1)
 ejemplo2 = main ([], exe2)
 ejemplo3 = main ([], exe3)
 ejemplo4 = main ([], exe4)
-ejemplo5 = main ([], exe5)   
-ejemplo6 = main ([], bueno)   
+ejemplo5 = main ([], exe5)
+ejemplo6 = main ([], bueno)
 ejemplo7 = main ([], exe6)
+
+p = V "p"
+q = V "q"
+r = V "r"
+s = V "s"
+t = V "t"
