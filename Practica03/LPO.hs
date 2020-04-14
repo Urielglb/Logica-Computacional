51312830efa5a2cc404e5b06b49a1377cacd5596
Valencia <davidalencia@gmail.com>
2020-04-13 19:13:01 -0500

    Practica 3 lista  | 19:13 04/13/20

--- Practica03/LPO.hs
+++ Practica03/LPO.hs
@@ -1,5 +1,5 @@
 {-
-- Logica Conmputacional 2020-2 
+- Logica Conmputacional 2020-2
 - Sustitucion para LPO.
 - Profesor: Favio Ezequiel Miranda Perea
 - Ayudante: Alejandra Krystel Coloapa Díaz
@@ -9,6 +9,7 @@ module LPO where
 
 --Modulo de listas brNombreados por el lenguaje
 import Data.List
+import Control.Exception
 
 --Tipo que representa el nombre.
 type Nombre = String
@@ -30,7 +31,14 @@ data Form = TrueF
           | Ex Nombre Form deriving(Show,Eq,Ord)
 
 -- | Subst. Tipo que representa una sustitución de variables en términos.
-type Subst = [(Nombre,Term)]          
+type Subst = [(Nombre,Term)]
+
+
+data MyException = FAIL
+instance Show MyException  where
+    show FAIL = " Sustitución no legal"
+
+instance Exception MyException
 
 -- | varT. Función que devuelve una lista con todos los índices de variables que
 -- figuran en t. (De regalo, les va a servir para apsubF en los casos de All y Ex)
@@ -39,7 +47,7 @@ varT (V x) = [x]
 varT (F f l) = foldl1 union [varT t | t <- l]
 
 auxVerifSus :: (Nombre,Term) -> Bool
-auxVerifSus (n,V t) 
+auxVerifSus (n,V t)
     | n == t = False
     | otherwise = True
 auxVerifSus (n, F t xs) = and [auxVerifSus (n, x) | x<-xs]
@@ -47,9 +55,9 @@ auxVerifSus (n, F t xs) = and [auxVerifSus (n, x) | x<-xs]
 -- | verifSus. Función que verifica una sustitución.
 verifSus :: Subst -> Bool
 verifSus [x] = auxVerifSus x
-verifSus(x:xs) 
+verifSus(x:xs)
     | verifSus [x] == False = False
-    | otherwise = verifSus xs 
+    | otherwise = verifSus xs
 
 -- | apsubT. Función que aplica una sustitución de variables de variables en términos
 -- en un término dado.
@@ -61,7 +69,7 @@ apsubT (V var) ((n, t):xs)
 apsubT (F fun terms) s = (F fun [ apsubT t s | t<-terms])
 
 
--- | apsubfuncion que devuelve la sustitucion de terminos en una formula 
+-- | apsubfuncion que devuelve la sustitucion de terminos en una formula
 apsubF :: Form -> Subst -> Form
 apsubF (TrueF) s = TrueF
 apsubF (FalseF) s = FalseF
@@ -69,10 +77,24 @@ apsubF (Pr n xs) s = Pr n [apsubT x s | x <-xs]
 apsubF (Eq a b) s = Eq (apsubT a s) (apsubT b s)
 apsubF (Neg f) s = Neg (apsubF f s)
 apsubF (Conj a b) s = Conj (apsubF a s) (apsubF b s)
-apsubF (Disy a b) s = Disy (apsubF a s) (apsubF b s) 
+apsubF (Disy a b) s = Disy (apsubF a s) (apsubF b s)
 apsubF (Imp a b) s = Imp (apsubF a s) (apsubF b s)
 apsubF (Equi a b) s= Equi (apsubF a s) (apsubF b s)
+apsubF a@(All nom form) s
+    | not (isInSubst nom s) = All nom (apsubF form s)
+    | otherwise = throw FAIL
+apsubF e@(Ex nom form) s
+    | not (isInSubst nom s) = Ex nom (apsubF form s)
+    | otherwise = throw FAIL
 
---apsubF All Nombre Form
---          | Ex Nombre Form 
+isInSubst :: Nombre -> Subst -> Bool
+isInSubst nom s = or [isInNomTerm nom x | x<-s]
 
+isInNomTerm :: Nombre->(Nombre, Term) -> Bool
+isInNomTerm nom (ns, ts) = or [nom==ns, or [t==nom | t<-varT ts]]
+
+x = V "x"
+a = (All "x"(Conj (Pr "Q"[V "m"]) (Pr "P"[V "w"])))
+s = [("w", V "z"),("m", F "a"[V "t"])]
+ex = (Ex "x"(Conj (Pr "Q"[V "x"]) (Pr "P"[V "w"])))
+s2 = [("w", V "x"),("m", F "a"[V "t"])]
